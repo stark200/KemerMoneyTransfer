f24d86307816848ff9a7be3daaaafe4be5433c4b
stark200 <33566254+stark200@users.noreply.github.com>
2022-10-29 19:18:26 +0300

    Add files via upload

---
+++ external.js
@@ -0,0 +1,12 @@
+function myFunction()
+{
+    document.location.href = "https://t.me/EthemKarabay";
+}
+function myFunction1()
+{
+    document.location.href = "https://api.whatsapp.com/send? phone=+905372655000";
+}
+function myFunction2()
+{
+    document.location.href = "https://yandex.ru/maps/-/CCUZ7NeXLB";
+}
