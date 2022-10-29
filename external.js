51e287787366bcdaf668fe4cda3e884d4dccb0d1
stark200 <33566254+stark200@users.noreply.github.com>
2022-10-29 22:31:08 +0300

    Update external.js

--- external.js
+++ external.js
@@ -10,3 +10,7 @@ function myFunction2()
 {
     document.location.href = "https://yandex.ru/maps/-/CCUZ7NeXLB";
 }
+function myFunction3()
+{
+    document.location.href = "https://goo.gl/maps/KHAZG2Lk735ia9jG8";
+}
